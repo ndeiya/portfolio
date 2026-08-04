e71c3fc724ea5dce4444d4f23dfbf2215d46521e
Abdul-Rahaman Abdulai <99042584+ndeiya@users.noreply.github.com>
2026-08-04 00:46:28 +0000

    Update portfolio entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <HelmetProvider><App /></HelmetProvider>
   </React.StrictMode>,
 )
