50f71abb5b1c119046385e62e26d42ae4cbe086b
Abdul-Rahaman Abdulai <99042584+ndeiya@users.noreply.github.com>
2026-08-04 06:12:38 +0000

    Support GitHub Pages project routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
 import './App.css'
 
+const redirectedPath = sessionStorage.getItem('portfolio-path')
+if (redirectedPath) {
+  sessionStorage.removeItem('portfolio-path')
+  window.history.replaceState(null, '', redirectedPath)
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider><App /></HelmetProvider>
